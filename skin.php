e69d39d4b0a118bfffa30a8416bcda634a5d53d5
Michael Paulukonis <xraysmalevich@gmail.com>
2013-05-21 23:18:39 -0400

    moved (commented out) darkstrap css from .tmpl to variably-set section on skin.php

--- skin.php
+++ skin.php
@@ -1,6 +1,5 @@
- <?php if (!defined('PmWiki')) exit();
+<?php if (!defined('PmWiki')) exit();
 /*  Copyright 2013 Michael Paulukonis
-    based on the work by gambiro
     This file is bootstrap-fluid.php; part of the bootstrap skin for pmwiki 2
     you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
@@ -8,7 +7,7 @@
     (at your option) any later version.
 */
 
-global $RecipeInfo, $SkinName, $SkinRecipeName, $WikiStyleApply, $PageLogoUrl;
+global $RecipeInfo, $SkinName, $SkinRecipeName, $WikiStyleApply, $PageLogoUrl, $HTMLHeaderFmt, $UseDarkstrapCss;
 # Some Skin-specific values
 $RecipeInfo['BootstrapSkin']['Version'] = '2013-05-20';
 $SkinName = 'bootstrap-fluid';
@@ -20,28 +19,40 @@ global $BootstrapSkin; $BootstrapSkin = 1;
 
 $PageLogoUrl = "$SkinDirUrl/images/ico/favicon.png";
 
+## you must populate $UseDarktstrapCSS in local/config.php
+## ROADMAP: instead of one variable, will able to choose between a variety of bootstrap themes (user-configurable)
+## cookie or something.
+if ($UseDarkstrapCss == 1) {
+        $HTMLHeaderFmt['option-css'] = "
+    <link href='$SkinDirUrl/css/darkstrap.css' rel='stylesheet'>
+    <!-- all customization should go in pmwiki.darkstrap.css -->
+    <link href='$SkinDirUrl/css/pmwiki.darkstrap.css' rel='stylesheet'>
+    ";
+}
+
+
 ## required for apply-actions
 $WikiStyleApply['link'] = 'a';  #allows A to be labelled with class attributes
 
 
 Markup('button', '>markupend',
        '/\\(:button(\\s+.*?)?:\\)/ei',
-  "SourceBlock(PSS('$1 '))");
+       "SourceBlock(PSS('$1 '))");
 
 function SourceBlock($args) {
 
-  $opt = ParseArgs($args);
+        $opt = ParseArgs($args);
 
-  // expect link, class, text
-  // newwin is optional; if not provided, or anything but "true" open in current window (default behavior)
-  $newwin = '';
-  if ($opt['newwin'] == 'true') {
-          $newwin = 'newwin';
-  }
+        // expect link, class, text
+        // newwin is optional; if not provided, or anything but "true" open in current window (default behavior)
+        $newwin = '';
+        if ($opt['newwin'] == 'true') {
+                $newwin = 'newwin';
+        }
 
-  $link = '[[%s|%s]]%%apply=link %s class=%s%%';
-  $linkf = sprintf($link, $opt['link'], $opt['text'], $newwin, $opt['class']);
+        $link = '[[%s|%s]]%%apply=link %s class=%s%%';
+        $linkf = sprintf($link, $opt['link'], $opt['text'], $newwin, $opt['class']);
 
-  return $linkf;
+        return $linkf;
 
 }
