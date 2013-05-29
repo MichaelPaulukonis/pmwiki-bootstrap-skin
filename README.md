b9d6eac4a2e7ec04712058753aacbc65f38397a9
Michael Paulukonis <xraysmalevich@gmail.com>
2013-05-28 23:40:06 -0400

    button code working, preliminary installation notes and screenshots in readme

--- README.md
+++ README.md
@@ -7,10 +7,30 @@ Based on the skin in the [Pmwiki Kit](https://github.com/gambhiro/pmwiki-kit-boo
 
 Notes also at [PmWiki Skins](http://www.pmwiki.org/wiki/Skins/TwitterBootstrap)
 
+# Installation
+Copy to /path/to/pwmiki/pub/skins/bootstrap-fluid
+
+Add the following to config.php:
+
+$Skin = 'bootstrap-fluid';
+
+To use the included darkstrap theme, add the following line:
+
+$UseDarkstrapCss = true;
+
+
+
+# Screenshots
+
+## Default theme
+![default screenshot](wiki/images/pmwiki.bootstrap.default.00.png)
+
+## Darkstrap theme
+![darkstrap screenshot](wiki/images/pmwiki.bootstrap.default.01.png)
 
 # Roadmap
 
-* Sample screenshots and markup documentation coming soon!
+* Markup documentation coming soon!
 * Better markup support for Bootstrap components.
 * Sample pages showing markup.
 * Documentation on how to add new Bootstrap themes.
--- skin.php
+++ skin.php
@@ -7,7 +7,9 @@
     (at your option) any later version.
 */
 
-global $RecipeInfo, $SkinName, $SkinRecipeName, $WikiStyleApply, $PageLogoUrl, $HTMLHeaderFmt, $UseDarkstrapCss;
+
+
+global $RecipeInfo, $SkinName, $SkinRecipeName, $WikiStyleApply, $PageLogoUrl, $HTMLHeaderFmt, $PageHeaderFmt, $PageNavStyle, $UseDarkstrapCss;
 # Some Skin-specific values
 $RecipeInfo['BootstrapSkin']['Version'] = '2013-05-20';
 $SkinName = 'bootstrap-fluid';
@@ -22,12 +24,24 @@ $PageLogoUrl = "$SkinDirUrl/images/ico/favicon.png";
 ## you must populate $UseDarktstrapCSS in local/config.php
 ## ROADMAP: instead of one variable, will able to choose between a variety of bootstrap themes (user-configurable)
 ## cookie or something.
+
+## NOTE: the light-theme's inverse (Dark) is the dark-theme's "normal"
+## so the below settings for navbar look the same
 if ($UseDarkstrapCss == 1) {
-        $HTMLHeaderFmt['option-css'] = "
-    <link href='$SkinDirUrl/css/darkstrap.css' rel='stylesheet'>
+        $HTMLHeaderFmt['option-css'] =
+    "<link href='$SkinDirUrl/css/darkstrap.css' rel='stylesheet'>
     <!-- all customization should go in pmwiki.darkstrap.css -->
-    <link href='$SkinDirUrl/css/pmwiki.darkstrap.css' rel='stylesheet'>
-    ";
+    <link href='$SkinDirUrl/css/pmwiki.darkstrap.css' rel='stylesheet'>";
+
+        $PageNavStyle =
+                "<div id='wikihead' class='navbar navbar-fixed-top'> ";
+
+
+} else {
+
+        $HTMLHeaderFmt['option-css'] = "";
+        $PageNavStyle =
+                "<div id='wikihead' class='navbar navbar-inverse navbar-fixed-top'>";
 }
 
 
@@ -35,7 +49,7 @@ if ($UseDarkstrapCss == 1) {
 $WikiStyleApply['link'] = 'a';  #allows A to be labelled with class attributes
 
 
-Markup('button', '>markupend',
+Markup('button', 'inline',
        '/\\(:button(\\s+.*?)?:\\)/ei',
        "SourceBlock(PSS('$1 '))");
 
@@ -56,3 +70,62 @@ function SourceBlock($args) {
         return $linkf;
 
 }
+
+function BootstrapButton($args) {
+
+        $opt = ParseArgs($args);
+
+        // expect link, class, text
+        // TODO: test for options
+        // TODO: handle alt params
+        // TODO: handle rel=nofollow per pmwiki settings
+
+        $linkf = '<a href="%s" class=%s>%s</a>';
+        $linkf = sprintf($link, $opt['link'], $opt['class'], $opt['text']);
+
+        return $linkf;
+
+}
+
+# the markup seems to work -- it's just that the CSS isn't finding the icon set...
+# also, supplying "class" seems needless...
+
+Markup('icon', 'inline',
+       '/\\(:icon(\\s+.*?)?:\\)/ei',
+       "BootstrapIcon(PSS('$1 '))");
+
+function BootstrapIcon($args) {
+
+        $icon = sprintf('<i class=%s ></i>', $args);
+
+        return $icon;
+}
+
+
+## added, not working. commenting out for the time being Saturday, May 25, 2013
+
+# attempt to set configs via actions....
+
+
+/* global $Now, $CookiePrefix, $DarkstrapCookie; */
+
+/* # set cookie expire time (default 1 year) */
+/* SDV($DarkstrapCookieExpires,$Now+60*60*24*365); */
+
+/* $prefix = $CookiePrefix.$SkinName.'_'; */
+
+/* SDV($SkinCookie, $prefix.'setskin'); */
+
+/* # darkstrap cookie routine */
+/* if($EnableDarkstrapOptions==1) { */
+/*     SDV($DarkstrapCookie, $prefix.'setcss'); */
+/*     if (isset($_COOKIE[$DarkstrapCookie])) $sf = $_COOKIE[$DarkstrapCookie]; */
+/*     if (isset($_GET['setcss'])) { */
+/*       $sf = $_GET['setcss']; */
+/*       setcookie($DarkstrapCookie,$sf,$DarkstrapCookieExpires,'/');} */
+/*     if (isset($_GET['css'])) $sf = $_GET['css']; */
+/*     if (@$PageDarkstrapList[$sf]) $DarkstrapCss = $PageDarkstrapList[$sf]; */
+/*     else $sf = $DefaultDarkstrap; */
+/* } */
+
+#####end cookies
